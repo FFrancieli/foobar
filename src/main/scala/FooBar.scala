c5f50a6061861a96755656ff222e759e177ffb5e
Francieli Viane <francielifrv@gmail.com>
2017-05-23 00:00:55 -0300

    Returns foo for any number divisible by 3

--- src/main/scala/FooBar.scala
+++ src/main/scala/FooBar.scala
@@ -1,6 +1,7 @@
 case class FooBar() {
 
   def calculate(number: Option[Int]) = number match {
+    case num if num.get % 3 == 0 => "foo"
     case _ => number.get
   }
 
--- src/test/scala/FooBarTest.scala
+++ src/test/scala/FooBarTest.scala
@@ -9,5 +9,19 @@ class FooBarTest extends WordSpec {
       assert(foobar.calculate(Some(1)) == 1)
       assert(foobar.calculate(Some(5)) == 5)
     }
+
+    "return foo" when {
+      "number is 3" in {
+        val foobar: FooBar = new FooBar()
+
+        assert(foobar.calculate(Some(3)) == "foo")
+      }
+
+      "number is divisible by 3" in {
+        val foobar: FooBar = new FooBar()
+
+        assert(foobar.calculate(Some(21)) == "foo")
+      }
+    }
   }
 }
